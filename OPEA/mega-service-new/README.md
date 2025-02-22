df0ec5c3a8d9091b293cc785dcd01604ae39cc98
Sura <sk@sura.me>
2025-02-22 14:43:28 -0500

    mega services from scratch with andrew and james week 3 following live

---
+++ OPEA/mega-service-new/README.md
@@ -0,0 +1,3 @@
+hack and learn with Andrew following session of week 3 live
+feb 22 2025
+https://www.youtube.com/@Examprochannel/live
---
+++ OPEA/mega-service-new/chat.py
@@ -0,0 +1,11 @@
+from comps import serviceOrchestrator, serviceRoleType, MicroService
+from comps.cores.proto.api_protocol 
+class Chat:
+    def __init__(self):
+        print('inti')
+        self.megaservice = ServiceOrchestrator
+        self.endpoint = 'sura-is-learning-endpoint'
+        self.port = 8000
+        self.host = '0.0.0.0'
+    def add_remote_service(self):
+        print('add_remote_service')
